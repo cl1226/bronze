e1a5f843bc87277a72d7e9be13de249f00f26323
chenlou <1061788688@qq.com>
2021-08-18 13:59:01 +0800

    add Normalizer

---
+++ bronze-core/src/main/scala/org/excitinglab/bronze/core/transform/Normalizer.scala
@@ -0,0 +1,56 @@
+package org.excitinglab.bronze.core.transform
+
+import org.apache.spark.ml.feature
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.excitinglab.bronze.apis.BaseTransform
+import org.excitinglab.bronze.config.{Config, ConfigFactory}
+
+class Normalizer extends BaseTransform {
+
+  var config: Config = ConfigFactory.empty()
+
+  override def process(spark: SparkSession, df: Dataset[Row]): Dataset[Row] = {
+    val normalizer = new feature.Normalizer()
+    normalizer.setInputCol(config.getString("inputCol"))
+    if (config.hasPath("outputCol")) {
+      normalizer.setOutputCol(config.getString("outputCol"))
+    }
+    normalizer.setP(config.getDouble("md"))
+
+    normalizer.transform(df)
+  }
+
+  /**
+   * Set Config.
+   * */
+  override def setConfig(config: Config): Unit = this.config = config
+
+  /**
+   * Get Config.
+   * */
+  override def getConfig(): Config = this.config
+
+  /**
+   * Return true and empty string if config is valid, return false and error message if config is invalid.
+   */
+  override def checkConfig(): (Boolean, String) = {
+    val requiredOptions = List("inputCol", "md")
+    val nonExistsOptions = requiredOptions.map(optionName => (optionName, config.hasPath(optionName))).filter { p =>
+      val (optionName, exists) = p
+      !exists
+    }
+    if (nonExistsOptions.nonEmpty) {
+      (
+        false,
+        "please specify " + nonExistsOptions
+          .map { option =>
+            val (name, exists) = option
+            "[" + name + "]"
+          }
+          .mkString(", ") + " as non-empty string"
+      )
+    } else {
+      (true, "")
+    }
+  }
+}
