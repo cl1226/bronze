401a40266ad32c401ea5a1be1f3d65f6987dfdb1
chenlou <1061788688@qq.com>
2021-08-19 16:32:01 +0800

    add GBTRegressor

---
+++ bronze-core/src/main/scala/org/excitinglab/bronze/core/validate/RegressionValidate.scala
@@ -0,0 +1,66 @@
+package org.excitinglab.bronze.core.validate
+
+import org.apache.spark.ml.PipelineModel
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.regression.GBTRegressionModel
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.excitinglab.bronze.apis.BaseValidate
+import org.excitinglab.bronze.config.{Config, ConfigFactory}
+
+class RegressionValidate extends BaseValidate {
+
+  var config: Config = ConfigFactory.empty()
+
+  override def process(spark: SparkSession, model: PipelineModel, df: Dataset[Row]): Dataset[Row] = {
+
+    val predictions = model.transform(df)
+
+    val evaluator = new RegressionEvaluator()
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
+      .setMetricName("rmse")
+
+    val rmse = evaluator.evaluate(predictions)
+    println(s">>>Root Mean Squared Error (RMSE) on test data = $rmse")
+
+    val model1 = config.getString("modelType") match {
+      case "GBTRegression" => model.stages.last.asInstanceOf[GBTRegressionModel]
+    }
+    println(s">>>Learned regression GBT model:\n ${model1.toDebugString}")
+
+    println(">>>预测结果: ")
+    predictions
+  }
+
+  /**
+   * Set Config.
+   * */
+  override def setConfig(config: Config): Unit = this.config = config
+
+  /**
+   * Get Config.
+   * */
+  override def getConfig(): Config = this.config
+
+  /**
+   * Return true and empty string if config is valid, return false and error message if config is invalid.
+   */
+  override def checkConfig(): (Boolean, String) = {
+    val requiredOptions = List("modelType")
+    val nonExistsOptions = requiredOptions
+      .map(optionName => (optionName, config.hasPath(optionName)))
+      .filter { p =>
+        val (optionName, exists) = p
+        !exists
+      }
+    nonExistsOptions.isEmpty match {
+      case true => (true, "")
+      case _ => {
+        (false, "please specify " + nonExistsOptions
+          .map{ case (field, _) => "[" + field + "]" }
+          .mkString(", ") + " as non-empty string")
+      }
+    }
+  }
+
+}
