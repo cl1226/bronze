df55ec5fb1bbd4508db8ba8f730953b47316b754
chenlou <1061788688@qq.com>
2021-08-23 14:18:42 +0800

    add Imputer

---
+++ bronze-core/src/main/scala/org/excitinglab/bronze/core/transform/Imputer.scala
@@ -0,0 +1,54 @@
+package org.excitinglab.bronze.core.transform
+
+import org.apache.spark.ml.feature
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.excitinglab.bronze.apis.BaseTransform
+import org.excitinglab.bronze.config.{Config, ConfigFactory}
+
+import scala.collection.JavaConversions._
+
+/**
+ * Inputer 缺失值补全
+ * 主要作用：对列中的NaN使用中位数或者平均数进行补全
+ */
+class Imputer extends BaseTransform {
+
+  var config: Config = ConfigFactory.empty()
+
+  /**
+   * Prepare before running, do things like set config default value, add broadcast variable, accumulator.
+   */
+  override def prepare(spark: SparkSession): Unit = {
+    super.prepare(spark)
+
+    val defaultConfig = ConfigFactory.parseMap(
+      Map(
+        "inputCol" -> "feature",
+        "outputCol" -> "imputer_feature"
+      )
+    )
+    config = config.withFallback(defaultConfig)
+  }
+
+  override def process(spark: SparkSession, df: Dataset[Row]): Dataset[Row] = {
+    val imputer = new feature.Imputer()
+      .setInputCols(config.getString("inputCol").split(",").map(_.trim))
+      .setOutputCols(config.getString("outputCol").split(",").map(_.trim))
+    imputer.fit(df).transform(df)
+  }
+
+  /**
+   * Set Config.
+   * */
+  override def setConfig(config: Config): Unit = this.config = config
+
+  /**
+   * Get Config.
+   * */
+  override def getConfig(): Config = this.config
+
+  /**
+   * Return true and empty string if config is valid, return false and error message if config is invalid.
+   */
+  override def checkConfig(): (Boolean, String) = (true, "")
+}
