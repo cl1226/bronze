7134d7e02831fc51ba0a0d74d3454abc8859e907
chenlou <1061788688@qq.com>
2021-08-23 14:12:38 +0800

    add Binarizer

---
+++ bronze-core/src/main/scala/org/excitinglab/bronze/core/transform/Binarizer.scala
@@ -0,0 +1,56 @@
+package org.excitinglab.bronze.core.transform
+
+import org.apache.spark.ml.feature
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.excitinglab.bronze.apis.BaseTransform
+import org.excitinglab.bronze.config.{Config, ConfigFactory}
+
+import scala.collection.JavaConversions._
+
+/**
+ * Binarizer 二值化器
+ * 主要作用：将数值特征阈值化为二进制（0/1）特征的过程
+ */
+class Binarizer extends BaseTransform {
+
+  var config: Config = ConfigFactory.empty()
+
+  /**
+   * Prepare before running, do things like set config default value, add broadcast variable, accumulator.
+   */
+  override def prepare(spark: SparkSession): Unit = {
+    super.prepare(spark)
+
+    val defaultConfig = ConfigFactory.parseMap(
+      Map(
+        "inputCol" -> "feature",
+        "outputCol" -> "binarized_feature",
+        "threshold" -> 0.0
+      )
+    )
+    config = config.withFallback(defaultConfig)
+  }
+
+  override def process(spark: SparkSession, df: Dataset[Row]): Dataset[Row] = {
+    val binarizer = new feature.Binarizer()
+      .setInputCol(config.getString("inputCol"))
+      .setOutputCol(config.getString("outputCol"))
+      .setThreshold(config.getDouble("threshold"))
+    binarizer.transform(df)
+  }
+
+  /**
+   * Set Config.
+   * */
+  override def setConfig(config: Config): Unit = this.config = config
+
+  /**
+   * Get Config.
+   * */
+  override def getConfig(): Config = this.config
+
+  /**
+   * Return true and empty string if config is valid, return false and error message if config is invalid.
+   */
+  override def checkConfig(): (Boolean, String) = (true, "")
+}
