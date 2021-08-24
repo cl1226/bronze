faa09e9d3492646f445949d16134ecbfdc757a37
chenlou <1061788688@qq.com>
2021-08-24 16:03:16 +0800

    add DecisionTreeRegression

--- bronze-core/src/main/scala/org/excitinglab/bronze/core/validate/RegressionValidate.scala
+++ bronze-core/src/main/scala/org/excitinglab/bronze/core/validate/RegressionValidate.scala
@@ -2,7 +2,7 @@ package org.excitinglab.bronze.core.validate
 
 import org.apache.spark.ml.PipelineModel
 import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.regression.GBTRegressionModel
+import org.apache.spark.ml.regression.{DecisionTreeRegressionModel, GBTRegressionModel}
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.excitinglab.bronze.apis.BaseValidate
 import org.excitinglab.bronze.config.{Config, ConfigFactory}
@@ -23,10 +23,18 @@ class RegressionValidate extends BaseValidate {
     val rmse = evaluator.evaluate(predictions)
     println(s">>>Root Mean Squared Error (RMSE) on test data = $rmse")
 
-    val model1 = config.getString("modelType") match {
-      case "GBTRegression" => model.stages.last.asInstanceOf[GBTRegressionModel]
+    config.getString("modelType") match {
+      case "GBTRegression" => {
+        val gbtModel = model.stages.last.asInstanceOf[GBTRegressionModel]
+        println(s">>>Learned regression GBT model:\n ${gbtModel.toDebugString}")
+      }
+      case "decisionTreeRegression" => {
+        val dtrModel = model.stages.last.asInstanceOf[DecisionTreeRegressionModel]
+        println(s">>>Learned regression tree model:\n ${dtrModel.toDebugString}")
+      }
+      case _ =>
     }
-    println(s">>>Learned regression GBT model:\n ${model1.toDebugString}")
+
 
     println(">>>预测结果: ")
     predictions
